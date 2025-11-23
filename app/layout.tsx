2f0d6b0dff90ba601c115a537040e253f451c69e
Jay <s223386489@deakin.edu.au>
2025-11-23 23:47:28 +1100

    Added web analytics with vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { CurrencyProvider } from '@/components/CurrencyContext';
 import "./globals.css";
 import { ToastProvider } from "@/components/ToastProvider";
@@ -52,6 +53,7 @@ export default function RootLayout({
           <ThemeProvider>
             {children}
             <ToastProvider />
+            <Analytics />
           </ThemeProvider>
         </CurrencyProvider>
       </body>
